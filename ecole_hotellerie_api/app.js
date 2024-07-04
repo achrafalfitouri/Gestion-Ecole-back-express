f034444121a872702631d1101de23b0912037a3e
achrafalfitouri <84252969+achrafalfitouri@users.noreply.github.com>
2024-07-04 20:28:31 +0100

    fromateru tswira n9i bzzf

--- ecole_hotellerie_api/app.js
+++ ecole_hotellerie_api/app.js
@@ -82,6 +82,23 @@ app.get('/api/etud/photo/:id', (req, res) => {
         return res.sendFile(photoFullPath);
     });
 });
+app.get('/api/form/photo/:id', (req, res) => {
+    const { id } = req.params;
+    const sql = "SELECT PhotoProfil FROM formateurs WHERE ID_Formateur = ?";
+    connection.query(sql, [id], (err, result) => {
+        if (err) {
+            console.error("Error retrieving photo profile:", err);
+            return res.status(500).json({ error: "Error retrieving photo profile" });
+        }
+        if (result.length === 0) {
+            return res.status(404).json({ error: "Student not found" });
+        }
+        const photoPath = result[0].PhotoProfil;
+        // Assuming the photos are stored in the 'upload/images' directory
+        const photoFullPath = path.join(__dirname, 'upload/images', photoPath);
+        return res.sendFile(photoFullPath);
+    });
+});
 
 
 
