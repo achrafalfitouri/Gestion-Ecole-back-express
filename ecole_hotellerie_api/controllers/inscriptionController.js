577681a107ccaef18f2c58f04f5aacb6dc692e5b
achrafalfitouri <84252969+achrafalfitouri@users.noreply.github.com>
2024-07-05 20:21:28 +0100

    5dam

--- ecole_hotellerie_api/controllers/inscriptionController.js
+++ ecole_hotellerie_api/controllers/inscriptionController.js
@@ -2,7 +2,25 @@ const connection = require('../config/db');
 
 // Get all inscriptions
 const getAllInscriptions = (req, res) => {
-    const sql = 'SELECT * FROM inscriptions ORDER BY created_at DESC';
+    const sql = `SELECT 
+    i.ID_Inscription,
+    i.ID_Etudiant,
+    i.DateDebutInscription,
+    i.DateFinInscription,
+    i.FraisInscription,
+    e.NomEtudiant,
+    e.ID_Filiere,
+    f.ID_Filiere,
+    f.NomFiliere
+FROM 
+    inscription i
+JOIN 
+    etudiants e ON i.ID_Etudiant = e.ID_Etudiant
+JOIN 
+    filiere f ON e.ID_Filiere = f.ID_Filiere
+ORDER BY 
+    i.created_at DESC;
+`;
     connection.query(sql, (err, results) => {
         if (err) return res.status(500).send(err.toString());
         res.send(results);
@@ -12,7 +30,7 @@ const getAllInscriptions = (req, res) => {
 // Get inscription by ID
 const getInscriptionById = (req, res) => {
     const { id } = req.params;
-    const sql = 'SELECT * FROM inscriptions WHERE ID_Inscription = ?';
+    const sql = 'SELECT * FROM inscription WHERE ID_Inscription = ?';
     connection.query(sql, [id], (err, results) => {
         if (err) return res.status(500).send(err.toString());
         if (results.length === 0) return res.status(404).send('Inscription not found');
@@ -22,9 +40,9 @@ const getInscriptionById = (req, res) => {
 
 // Create new inscription
 const createInscription = (req, res) => {
-    const { ID_Etudiant, DateDebutInscription, DateFinInscription, FraisInscription, MontantInscription } = req.body;
-    const sql = 'INSERT INTO inscriptions (ID_Etudiant, DateDebutInscription, DateFinInscription, FraisInscription, MontantInscription, created_at, updated_at) VALUES (?, ?, ?, ?, ?, NOW(), NOW())';
-    connection.query(sql, [ID_Etudiant, DateDebutInscription, DateFinInscription, FraisInscription, MontantInscription], (err, result) => {
+    const { ID_Etudiant, DateDebutInscription, DateFinInscription, FraisInscription } = req.body;
+    const sql = 'INSERT INTO inscription (ID_Etudiant, DateDebutInscription, DateFinInscription, FraisInscription, created_at, updated_at) VALUES (?, ?, ?, ?, NOW(), NOW())';
+    connection.query(sql, [ID_Etudiant, DateDebutInscription, DateFinInscription, FraisInscription], (err, result) => {
         if (err) return res.status(500).send(err.toString());
         res.send('Inscription created successfully!');
     });
@@ -33,9 +51,9 @@ const createInscription = (req, res) => {
 // Update inscription by ID
 const updateInscriptionById = (req, res) => {
     const { id } = req.params;
-    const { ID_Etudiant, DateDebutInscription, DateFinInscription, FraisInscription, MontantInscription } = req.body;
-    const sql = 'UPDATE inscriptions SET ID_Etudiant = ?, DateDebutInscription = ?, DateFinInscription = ?, FraisInscription = ?, MontantInscription = ?, updated_at = NOW() WHERE ID_Inscription = ?';
-    connection.query(sql, [ID_Etudiant, DateDebutInscription, DateFinInscription, FraisInscription, MontantInscription, id], (err, result) => {
+    const { ID_Etudiant, DateDebutInscription, DateFinInscription, FraisInscription } = req.body;
+    const sql = 'UPDATE inscription SET ID_Etudiant = ?, DateDebutInscription = ?, DateFinInscription = ?, FraisInscription = ?,  updated_at = NOW() WHERE ID_Inscription = ?';
+    connection.query(sql, [ID_Etudiant, DateDebutInscription, DateFinInscription, FraisInscription, id], (err, result) => {
         if (err) return res.status(500).send(err.toString());
         if (result.affectedRows === 0) return res.status(404).send('Inscription not found');
         res.send('Inscription updated successfully!');
@@ -45,7 +63,7 @@ const updateInscriptionById = (req, res) => {
 // Delete inscription by ID
 const deleteInscriptionById = (req, res) => {
     const { id } = req.params;
-    const sql = 'DELETE FROM inscriptions WHERE ID_Inscription = ?';
+    const sql = 'DELETE FROM inscription WHERE ID_Inscription = ?';
     connection.query(sql, [id], (err, result) => {
         if (err) return res.status(500).send(err.toString());
         if (result.affectedRows === 0) return res.status(404).send('Inscription not found');
