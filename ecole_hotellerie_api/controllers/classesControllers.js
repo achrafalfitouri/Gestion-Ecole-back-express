813b99f92d331f0dbd05140d5dced891715cae9e
achrafalfitouri <84252969+achrafalfitouri@users.noreply.github.com>
2024-07-09 19:13:08 +0100

    ccccc

--- ecole_hotellerie_api/controllers/classesControllers.js
+++ ecole_hotellerie_api/controllers/classesControllers.js
@@ -3,7 +3,15 @@ const connection = require('../config/db');
 // Get all classes
 // Get all classes
 const getAllclasses = (req, res) => {
-    const sql = `SELECT c.ID_Classe, c.NomClasse, c.ID_Filiere, c.ID_AnneeScolaire, c.Remarques, f.NomFiliere, a.AnneeScolaire, GROUP_CONCAT(CONCAT('(', e.NumEtudiant, ') ', e.NomEtudiant, ' ', e.PrenomEtudiant) ORDER BY e.NumEtudiant SEPARATOR '\n') AS NomComplet FROM classes c JOIN filiere f ON c.ID_Filiere = f.ID_Filiere JOIN anneeScolaire a ON a.ID_AnneeScolaire = c.ID_AnneeScolaire JOIN etudiants e ON c.ID_Classe = e.ID_Classe GROUP BY c.ID_Classe, c.NomClasse, c.ID_Filiere, c.ID_AnneeScolaire, c.Remarques, f.NomFiliere, a.AnneeScolaire ORDER BY c.created_at DESC;
+    const sql = `SELECT c.ID_Classe, c.NomClasse,C.Groupe, c.ID_Filiere, c.ID_AnneeScolaire, c.Remarques, f.NomFiliere, a.AnneeScolaire, GROUP_CONCAT(CONCAT('(', e.NumEtudiant, ') ', e.NomEtudiant, ' ', e.PrenomEtudiant) ORDER BY e.NumEtudiant SEPARATOR '\n') AS NomComplet FROM classes c JOIN filiere f ON c.ID_Filiere = f.ID_Filiere JOIN anneeScolaire a ON a.ID_AnneeScolaire = c.ID_AnneeScolaire JOIN etudiants e ON c.ID_Classe = e.ID_Classe GROUP BY c.ID_Classe, c.NomClasse, c.ID_Filiere, c.ID_AnneeScolaire, c.Remarques, f.NomFiliere, a.AnneeScolaire ORDER BY c.created_at DESC;
+`;
+    connection.query(sql, (err, results) => {
+        if (err) return res.status(500).send(err.toString());
+        res.send(results);
+    });
+};
+const getAllclasses2 = (req, res) => {
+    const sql = `SELECT c.ID_Classe, c.NomClasse,c.Groupe, c.ID_Filiere, c.ID_AnneeScolaire, c.Remarques, f.NomFiliere, a.AnneeScolaire FROM classes c JOIN filiere f ON c.ID_Filiere = f.ID_Filiere JOIN anneeScolaire a ON a.ID_AnneeScolaire = c.ID_AnneeScolaire GROUP BY c.ID_Classe, c.NomClasse, c.ID_Filiere, c.ID_AnneeScolaire,c.Groupe, c.Remarques, f.NomFiliere, a.AnneeScolaire ORDER BY c.created_at DESC;
 `;
     connection.query(sql, (err, results) => {
         if (err) return res.status(500).send(err.toString());
@@ -24,9 +32,9 @@ const getclassesById = (req, res) => {
 
 // Create new class
 const createclasses = (req, res) => {
-    const { NomClasse, ID_Filiere, ID_AnneeScolaire, Remarques } = req.body;
-    const sql = 'INSERT INTO classes (NomClasse, ID_Filiere, ID_AnneeScolaire, Remarques, created_at, updated_at) VALUES (?, ?, ?, ?, NOW(), NOW())';
-    connection.query(sql, [NomClasse, ID_Filiere, ID_AnneeScolaire, Remarques], (err, result) => {
+    const { NomClasse, ID_Filiere,Groupe, ID_AnneeScolaire, Remarques } = req.body;
+    const sql = 'INSERT INTO classes (NomClasse, ID_Filiere,Groupe, ID_AnneeScolaire, Remarques, created_at, updated_at) VALUES (?,?, ?, ?, ?, NOW(), NOW())';
+    connection.query(sql, [NomClasse, ID_Filiere,Groupe, ID_AnneeScolaire, Remarques], (err, result) => {
         if (err) return res.status(500).send(err.toString());
         res.send('Class created successfully!');
     });
@@ -35,9 +43,9 @@ const createclasses = (req, res) => {
 // Update class by ID
 const updateclassesById = (req, res) => {
     const { id } = req.params;
-    const { NomClasse, ID_Filiere, ID_AnneeScolaire, Remarques } = req.body;
-    const sql = 'UPDATE classes SET NomClasse = ?, ID_Filiere = ?, ID_AnneeScolaire = ?, Remarques = ?, updated_at = NOW() WHERE ID_Classe = ?';
-    connection.query(sql, [NomClasse, ID_Filiere, ID_AnneeScolaire, Remarques, id], (err, result) => {
+    const { NomClasse, ID_Filiere, ID_AnneeScolaire,Groupe, Remarques } = req.body;
+    const sql = 'UPDATE classes SET NomClasse = ?, ID_Filiere = ?,Groupe= ?, ID_AnneeScolaire = ?, Remarques = ?, updated_at = NOW() WHERE ID_Classe = ?';
+    connection.query(sql, [NomClasse, ID_Filiere, ID_AnneeScolaire,Groupe, Remarques, id], (err, result) => {
         if (err) return res.status(500).send(err.toString());
         if (result.affectedRows === 0) return res.status(404).send('Class not found');
         res.send('Class updated successfully!');
@@ -60,5 +68,6 @@ module.exports = {
     getclassesById,
     createclasses,
     updateclassesById,
-    deleteclassesById
+    deleteclassesById,
+    getAllclasses2
 };
--- ecole_hotellerie_api/routes/classeRoutes.js
+++ ecole_hotellerie_api/routes/classeRoutes.js
@@ -1,10 +1,11 @@
 
 const express = require('express');
 const router = express.Router();
-const {  getAllclasses, getclassesById, createclasses, updateclassesById, deleteclassesById} = require('../controllers/classesControllers');
+const {  getAllclasses, getclassesById, createclasses, updateclassesById, deleteclassesById, getAllclasses2} = require('../controllers/classesControllers');
 const authenticateJWT = require('../middleware/auth');
 
 router.get('/', authenticateJWT, getAllclasses);
+router.get('/all', authenticateJWT, getAllclasses2);
 router.get('/:id', authenticateJWT, getclassesById);
 router.post('/', authenticateJWT, createclasses);
 router.put('/:id', authenticateJWT, updateclassesById);
